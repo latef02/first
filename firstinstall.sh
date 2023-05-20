b8ec924f517123e2a43f568cde1d181b3f888b67
latef02 <milanjack2@gmail.com>
2023-05-20 23:08:11 +0100

    tes

---
+++ firstinstall.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+echo "install alacritty" 
+sudo pacman -S alacritty
+echo "installing chromium" 
+sudo pacman -S chromium
+echo "installing sxhkd" 
+sudo pacman -S sxhkd
+echo "installing redshift"
+sudo pacman -S redshift 
+echo "intstalling mpv"
+sudo pacman -S mpv 
+echo "instqlling youtube-dl"
+sudo pacman -S youtube-dl
+echo "isntqlling xclip"
+sudo pacman -S xclip
+echo "installing pulseaudio"
+sudo pacman -S pulseaudio
+echo "installing pamixer"
+sudo pacman -S pamixer
+echo "instqlling sound alsa"
+sudo pacman -S alsa-utils
+echo "installing spotify-launcher
+sudo pacman -S spotify-launcher
+echo "installing telegram"
+sudo pacman -S  telegram-desktop
+echo "install rofi"
+sudo pacman install rofi
+echo "installing hexchat
+sudo pacman -S hexchat
+echo "installing gparted"
+sudo pacman -S gparted
+echo "installing thunar"
+sudo pacman -S thunar
+echo "installing gedit"
+sudo pacman -S gedit
+echo "installing htop"
+sudo pacman -s htop
+echo "installing feh"
+sudo pacman -S feh
+echo "installing neofetch"
+sudo pacman -S neofetch
+echo "installing lf"
+sudo pacman -S lf
+echo "installing sxiv"
+sudo pacman -S sxiv
+echo "installing ueberzug"
+sudo pacman -S ueberzug
+echo "installing firefox"
+sudo pacman -S firefox
+echo "installing libreoffice"
+sudo pacman -S libreoffice
+echo "installing wget"
+sudo pacman -S wget
+echo "installing ffmpegthumbnailer"
+sudo pacman -S ffmpegthumbnailer
+echo "installing bat"
+sudo pacman -S bat
+echo "installing maim"
+sudo pacman -S maim
+echo "installing joypixel ttf "
+sudo pacman -S  ttf-joypixels
+echo "installing dusnt for notification"
+sudo pacman -S dunst
+
